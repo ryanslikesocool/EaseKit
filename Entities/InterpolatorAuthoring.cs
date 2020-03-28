12ddb64a7f31a250871d8c24b5bdf5c2cb6ecffa
ryan <ryanjboyer@gmail.com>
2020-03-27 20:37:45 -0600

    Added namespace for InterpolatorAuthoring.cs

--- Entities/InterpolatorAuthoring.cs
+++ Entities/InterpolatorAuthoring.cs
@@ -1,20 +1,23 @@
-﻿using Unity.Entities;
+using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
 using ifelse.Easings;
 
-[DisallowMultipleComponent]
-[RequiresEntityConversion]
-public class InterpolatorAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+namespace ifelse.Easings.Entities
 {
-    public EasingType function;
-    public float initial;
-    public float target;
-    public float duration;
-    public bool removeOnDone;
-
-    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    [DisallowMultipleComponent]
+    [RequiresEntityConversion]
+    public class InterpolatorAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     {
-        dstManager.AddComponentData(entity, new ifelse.Easings.Entities.Interpolator(initial, target, duration, function, removeOnDone));
+        public EasingType function;
+        public float initial;
+        public float target;
+        public float duration;
+        public bool removeOnDone;
+
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            dstManager.AddComponentData(entity, new ifelse.Easings.Entities.Interpolator(initial, target, duration, function, removeOnDone));
+        }
     }
 }
