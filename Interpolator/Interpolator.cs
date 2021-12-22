afa2b72a0e9da9873d2eb7489ff1e1f6b32111a3
Ryan Boyer <ryanjboyer@gmail.com>
2021-12-21 21:51:21 -0700

    Update Interpolator.cs

--- Interpolator/Interpolator.cs
+++ Interpolator/Interpolator.cs
@@ -32,7 +32,7 @@ namespace Easings.Interpolator {
         }
 
         /// <summary>
-        /// Start an easing animation starting with [startValue] and ending at [endValue] over [duration] (with optionally [unscaledTime]) interpolated using the [easing] function, calling [onUpdate] every frame, and optionall [onDone] when complete.
+        /// Start an easing animation starting with [startValue] and ending at [endValue] over [duration] (with optionally [unscaledTime]) interpolated using the [easing] function, calling [onUpdate] every frame, and optional [onDone] when complete.
         /// </summary>
         /// <param name="startValue">The value to start the ease at.</param>
         /// <param name="endValue">The value to end the ease at.</param>
@@ -45,7 +45,20 @@ namespace Easings.Interpolator {
         public static Coroutine Ease(float startValue, float endValue, float duration, EasingType easing, Action<EaseData> onUpdate, Action onDone = null, bool unscaledTime = false) => Ease(startValue, endValue, duration, easing, () => unscaledTime ? Time.unscaledDeltaTime : Time.deltaTime, onUpdate, onDone);
 
         /// <summary>
-        /// Start an easing animation starting with 0 and ending at 1 over [duration] (with optionally [unscaledTime]) interpolated using the [easing] function, calling [onUpdate] every frame, and optionall [onDone] when complete.
+        /// Start an easing animation starting with [startValue] and ending at [endValue] over [duration] (with optionally [unscaledTime]) interpolated using the [easing] function, calling [onUpdate] every frame, and optional [onDone] when complete.
+        /// </summary>
+        /// <param name="startValue">The value to start the ease at.</param>
+        /// <param name="endValue">The value to end the ease at.</param>
+        /// <param name="duration">The ease [duration].</param>
+        /// <param name="easing">The predefined [easing] type to use.</param>
+        /// <param name="deltaTime">Use a custom [deltaTime] each frame.</param>
+        /// <param name="onUpdate">The function to call every frame, passing in the current EaseData.</param>
+        /// <param name="onDone">The optional function to call when the easing is done.</param>
+        /// <returns>Coroutine instance in case stopping is needed.</returns>
+        public static Coroutine Ease(float startValue, float endValue, float duration, EasingType easing, Func<float> deltaTime, Action<EaseData> onUpdate, Action onDone = null, bool unscaledTime = false) => Ease(startValue, endValue, duration, easing, deltaTime, onUpdate, onDone);
+
+        /// <summary>
+        /// Start an easing animation starting with 0 and ending at 1 over [duration] (with optionally [unscaledTime]) interpolated using the [easing] function, calling [onUpdate] every frame, and optional [onDone] when complete.
         /// </summary>
         /// <param name="duration">The ease [duration].</param>
         /// <param name="easing">The predefined [easing] type to use.</param>
@@ -54,6 +67,17 @@ namespace Easings.Interpolator {
         /// <param name="unscaledTime">Use Time.unscaledDeltaTime if true.  Otherwise use Time.deltaTime.</param>
         /// <returns>Coroutine instance in case stopping is needed.</returns>
         public static Coroutine Ease(float duration, EasingType easing, Action<EaseData> onUpdate, Action onDone = null, bool unscaledTime = false) => Ease(0, 1, duration, easing, () => unscaledTime ? Time.unscaledDeltaTime : Time.deltaTime, onUpdate, onDone);
+
+        /// <summary>
+        /// Start an easing animation starting with 0 and ending at 1 over [duration] (updating by [deltaTime]) interpolated using the [easing] function, calling [onUpdate] every frame, and optional [onDone] when complete.
+        /// </summary>
+        /// <param name="duration">The ease [duration].</param>
+        /// <param name="easing">The predefined [easing] type to use.</param>
+        /// <param name="deltaTime">Use a custom [deltaTime] each frame.</param>
+        /// <param name="onUpdate">The function to call every frame, passing in the current EaseData.</param>
+        /// <param name="onDone">The optional function to call when the easing is done.</param>
+        /// <returns>Coroutine instance in case stopping is needed.</returns>
+        public static Coroutine Ease(float duration, EasingType easing, Func<float> deltaTime, Action<EaseData> onUpdate, Action onDone = null) => Ease(0, 1, duration, easing, deltaTime, onUpdate, onDone);
 
         private static Coroutine Ease(float startValue, float endValue, float duration, EasingType easing, Func<float> deltaTime, Action<EaseData> onUpdate, Action onDone = null) {
             return Shared.StartCoroutine(Ease());
